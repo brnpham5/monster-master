38a0b6002324dbe7dc41eb07d378f0d73cfc6d90
brnpham5 <bpham5@horizon.csueastbay.edu>
2017-02-20 16:13:20 -0800

    Minor Fix
    Fix to Monsters and Spells:
    Axe Raider
    Red Eyes Black Dragon
    Skull Red Bird
    
    Raimei
    
    changed import package yugiMonster/yugiSpell to YugiMonster/YugiSpell.

--- MonsterMaster/src/cards/YugiSpells/Hinotama.java
+++ MonsterMaster/src/cards/YugiSpells/Hinotama.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cards.yugiSpells;
+package cards.YugiSpells;
 
 import game.playerPackage.player;
 import game.playerPackage.YugiPlayer;
